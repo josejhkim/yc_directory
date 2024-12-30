2f5fd1ba56bbe68ca80a04ecc88641f70c22cda5
Jose <barsamic03@gmail.com>
2024-12-30 14:38:22 +0900

    Enable live reload for new Sanity documents

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import SearchForm from "../../components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({ searchParams } : {
   searchParams: Promise<{query?: string}>
@@ -10,7 +10,9 @@ export default async function Home({ searchParams } : {
 
   const query = (await searchParams).query;
 
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const {data: posts} = await sanityFetch({
+    query: STARTUPS_QUERY
+  });
 
   console.log(JSON.stringify(posts, null, 2));
 
@@ -38,6 +40,7 @@ export default async function Home({ searchParams } : {
           )}
         </ul>
       </section>
+      <SanityLive/>
     </>
   );
 }

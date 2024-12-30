f0d4bef8a162cc6dcf896f419545df441149280e
Jose <barsamic03@gmail.com>
2024-12-30 15:03:48 +0900

    Create params for Sanity query

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,9 +9,11 @@ export default async function Home({ searchParams } : {
 }) {
 
   const query = (await searchParams).query;
+  const params = { search: query || null};
 
   const {data: posts} = await sanityFetch({
-    query: STARTUPS_QUERY
+    query: STARTUPS_QUERY,
+    params
   });
 
   console.log(JSON.stringify(posts, null, 2));

f31402677811399f05383fb3823e49d4a9a5adad
Jose <barsamic03@gmail.com>
2025-01-04 22:33:46 +0900

    Add create startup server action

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -12,6 +12,7 @@ import {z} from "zod";
 import { useToast } from '@/hooks/use-toast';
 import { Result } from 'postcss';
 import { useRouter } from 'next/navigation';
+import { createPitch } from '@/lib/actions';
 
 const StartupForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({});
@@ -31,18 +32,16 @@ const StartupForm = () => {
 
       await formSchema.parseAsync(formValues);
       
-      // const result = await createIdea(prevstate, formData, pitch);
+      const result = await createPitch(prevstate, formData, pitch);
 
-      // console.log(result);
+      if (result.status == "SUCCESS") {
+        toast({
+          title: "Success",
+          description: "Your startup pitch has been created successfully",
+        });
 
-      // if (result.status == "SUCCESS") {
-      //   toast({
-      //     title: "Success",
-      //     description: "Your startup pitch has been created successfully",
-      //   });
-
-      //   router.push(`/startup/${result.id}`);
-      // }
+        router.push(`/startup/${result._id}`);
+      }
 
     } catch (error) {
       if (error instanceof z.ZodError) {

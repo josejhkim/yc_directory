c65b47d85c73d3b208b617cf15d1fbd29ebd0243
Jose <barsamic03@gmail.com>
2025-01-04 17:29:02 +0900

    Disable caching for fetching user info

--- auth.ts
+++ auth.ts
@@ -10,7 +10,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     async signIn({
       user: { name, email, image },
       profile: { id, login, bio } }) {
-      const existingUser = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+      const existingUser = await client.withConfig({useCdn: false}).fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
         id: id,
       });
 
@@ -31,7 +31,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 
     async jwt({ token, account, profile }) {
       if (account && profile) {
-        const user = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, { id: profile?.id });
+        const user = await client.withConfig({useCdn: false}).fetch(AUTHOR_BY_GITHUB_ID_QUERY, { id: profile?.id });
 
         token.id = user?._id;
       }
